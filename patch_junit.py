3ef1593e7e6e7a77eaea803a78e1561a5208eef9
Tony Holroyd <tony.j.holroyd@gmail.com>
2019-06-29 20:10:35 +0100

    make the patched test cases more accurate

--- patch_junit.py
+++ patch_junit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import xml.etree.ElementTree as ET
+import copy
 
 
 def flip_results(expected, actual):
@@ -12,8 +13,14 @@ def flip_results(expected, actual):
                 for actual_case in actual_suite.findall("./testcase[@name='{}']".format(expected_case_name)):
                     if expected_case.attrib['status'] == actual_case.attrib['status']:
                         actual_case.attrib['status'] = 'PASSED'
+                        for failure_message in actual_case.findall('failure'):
+                            actual_case.remove(failure_message)
+                            actual_case.text = None
                     else:
-                        actual_case.attrib['status'] == 'FAILED'
+                        new_case = copy.deepcopy(expected_case)
+                        new_case.attrib['status'] = 'FAILED'
+                        actual_suite.remove(actual_case)
+                        actual_suite.append(new_case)
 
         for actual_suite in actual.findall("testsuite"):
             failed = actual_suite.findall("./testcase[@status='FAILED']")
@@ -22,20 +29,28 @@ def flip_results(expected, actual):
     failed_tests = actual.findall("./testsuite/testcase[@status='FAILED']")
     actual.attrib['failures'] = str(len(failed_tests))
 
-    print(ET.tostring(actual, encoding='UTF-8').decode('UTF-8'))
 
-
-
-def main(source_file_name, expected_file_name, destination_file_name):
-    actual_tree = ET.parse(source_file_name)
+def main(source, expected, destination):
+    actual_tree = ET.parse(source)
     actual_root = actual_tree.getroot()
 
-    expected_tree = ET.parse(expected_file_name)
+    expected_tree = ET.parse(expected)
     expected_root = expected_tree.getroot()
 
     flip_results(expected_root, actual_root)
 
+    destination.write(ET.tostring(actual_root, encoding='UTF-8').decode('UTF-8'))
 
 
 if __name__ == '__main__':
-    main("junit_STS1.xml", "expected.xml", "destination.xml")
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Change the results in a JUnit file')
+    parser.add_argument('original_file', type=argparse.FileType("r"))
+    parser.add_argument('expected_file', type=argparse.FileType("r"))
+    parser.add_argument('destination_file', type=argparse.FileType("w"), default='-', nargs='?')
+
+    args = parser.parse_args()
+    main(args.original_file, args.expected_file, args.destination_file)
+
+    args.destination_file.close()
